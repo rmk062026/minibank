5a48aeb4ef04bd9d8582103848286d91b43c3471
rmk062026 <rmk062026@gmail.com>
2026-07-30 12:54:42 +0200

    Made first draw for accounts and bank system

--- Program.cs
+++ Program.cs
@@ -1,1 +1,48 @@
 ﻿Console.WriteLine("Mini Bank");
+
+List<string> userNames = new List<string>
+{
+    "Admin",
+    "Ola",
+    "Nordmann"
+};
+
+List<int> pinCodes = new List<int>
+{
+    1234,
+    3452,
+    8372
+};
+
+List<decimal> balances = new List<decimal>
+{
+    834532,
+    5325,
+    29342
+};
+
+Console.WriteLine("Welcome. Please login...");
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine()!;
+
+Console.WriteLine($"Hello {userName}, Enter your pincode:");
+
+// Username as int to find index
+int userIndex = userNames.IndexOf(userName);
+
+if (int.TryParse(Console.ReadLine(), out int pinCode))
+{
+    if (userIndex != -1 && pinCode == pinCodes[userIndex])
+    {
+        Console.WriteLine($"Account name: {userNames[userIndex]}");
+        Console.WriteLine($"Balance: {balances[userIndex]}");
+    }
+    else
+    {
+        Console.WriteLine("Account not found...");
+    }
+}
+else
+{
+    Console.WriteLine("PIN must be a number...");
+}
